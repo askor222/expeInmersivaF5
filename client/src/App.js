d1699f528dee4035619b15f24205900747753a58
Greiskel <greiskel.sanchez@gmail.com>
2023-09-06 09:30:55 +0200

    Actualización de assets

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import InmersiveExpe from './routes/InmersiveExpe';
 
+
 function App() {
   return (
     <>
--- client/src/routes/InmersiveExpe.js
+++ client/src/routes/InmersiveExpe.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import Inmersive from '../components/Inmersive';
+import Inmersive from '../components/Inmersive/Inmersive';
 
 const InmersiveExpe = () => {
+  
   return (
-    <div> Inmersive
+    <div>
         <h1>Inmersive</h1>
 <Inmersive />
     </div>
