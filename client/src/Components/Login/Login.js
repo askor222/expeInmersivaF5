89723b59fb6a1c58a2ec75647f5ad24489b4cc94
Marcela3290 <natisevo25@gmail.com>
2023-09-06 10:09:06 +0200

    navbar footer

---
+++ client/src/Components/Login/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import Navbar from '../Navbar/Navbar';
+import Logo from '../Logo/Logo';
+import './Login.css';
+
+function Login() {
+  return (
+    <div>
+    
+      <Navbar />
+     
+      <Footer />
+      <Logo />
+      
+      </div>
+  );
+}
+
+export default Login;
---
+++ client/src/Components/Logo/Logo.js
@@ -0,0 +1,20 @@
+
+import 'aframe';
+import './Logo.css';
+
+function Logo() {
+
+  return (
+    <a-scene embedded>
+      <a-entity position="-3.56849 0.7934 -2.38507" rotation="90 30 2" scale="8.1 8.1 8.1" animation-mixer="action" play-all-model-animations=""
+        gltf-model="https://cdn.glitch.global/c55c420c-bc3f-41b6-858e-f2e36523fc3c/sol3.glb?v=1693482539125"></a-entity>
+      <a-entity position="-2.47938 0.80536 -2.16517" rotation="10 20 30" scale="17 17 17" animation-mixer="action" play-all-model-animations=""
+        gltf-model="https://cdn.glitch.global/c55c420c-bc3f-41b6-858e-f2e36523fc3c/flecha.glb?v=1693483410512"></a-entity>
+
+
+    </a-scene>
+
+  );
+}
+
+export default Logo;
