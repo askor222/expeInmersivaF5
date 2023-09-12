eeb530e02ae0547df6a08cb95c71b854c4c39870
Greiskel <greiskel.sanchez@gmail.com>
2023-09-12 17:22:03 +0200

    login

--- client/src/Components/header/Header.jsx
+++ client/src/Components/header/Header.jsx
@@ -17,15 +17,17 @@ function Navbar() {
         </div>
         <div className={`links ${clicked ? 'active' : ''}`} style={{ width: clicked ? '100%' : '0' }}>
           <a onClick={handleClick} href="/">Home</a>
-          <a onClick={handleClick} href="/SobreF5">Sobre F5</a>
-          <a onClick={handleClick} href="#h">Contáctanos</a>
-          <a onClick={handleClick} href="#h">Experiencia360</a>
+          <a onClick={handleClick} href="/">Somos F5</a>
+          <a onClick={handleClick} href="/">Factoria F5</a>
+          <a onClick={handleClick} href="/Contact">Contáctanos</a>
+          <a onClick={handleClick} href="/">Experiencia360</a>
         </div>
         <div className={`links-right ${clicked ? 'active' : ''}`}>
           <a href="/">Home</a>
           <a href="/SobreF5">Sobre F5</a>
-          <a href="#h">Contáctanos</a>
-          <a href="#h">Experiencia360</a>
+          <a href="Contact">Factoria F5</a>
+          <a href="Contact">Contáctanos</a>
+          <a href="Contact">Experiencia360</a>
         </div>
         <div className="burguer">
           <BurguerButton clicked={clicked} handleClick={handleClick} />
